7f020d33d77d8922bcd444120e90c15f3f7cc790
Eduardo Talavera <edgar.talavera@ion.com.mx>
2025-05-24 11:12:33 -0600

    fix(avatar): export avatar component

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ export * from './components/Button/Button'
 export * from './components/Toggle/Toggle'
 export * from './components/Table/Table'
 export * from './components/Input/Input'
+export * from './components/Avatar/Avatar'
 
 // Context and providers
 export * from './context/ThemeContext'
